cfc08e24faded8d56e8f47dfe969bb2b9e7148c8
itslilito <129760498+itslilito@users.noreply.github.com>
2024-05-27 14:55:49 -0300

    Responsive (portada + ctas)
    Hice responsive la portada y los ctas correspondientes

--- scriptCTAsPPrincipal.js
+++ scriptCTAsPPrincipal.js
@@ -1,8 +1,12 @@
 var cta_inicial = document.getElementById("cta-i");
 var cta_final = document.getElementById("cta-f");
+var cta_mobile_inicial = document.getElementById("cta-m-i");
+var cta_mobile_final = document.getElementById("cta-m-f");
 
 cta_inicial.addEventListener("click", pasarAForm);
 cta_final.addEventListener("click", pasarAForm);
+cta_mobile_inicial.addEventListener("click", pasarAForm);
+cta_mobile_final.addEventListener("click", pasarAForm);
 
 function pasarAForm (event){
     window.location.href = "./form.html";
