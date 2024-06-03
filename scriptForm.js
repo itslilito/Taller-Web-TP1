74ced596eef4fddd8eb53bc870d88a5e0de34602
itslilito <129760498+itslilito@users.noreply.github.com>
2024-06-03 08:47:40 -0300

    FUNCIONAN LOS DATOOOOOOS

--- scriptForm.js
+++ scriptForm.js
@@ -6,6 +6,7 @@ var flechita_atras = document.getElementById("flechita-atras");
 var flechita_atras_mobile = document.getElementById("flechita-atras-mobile");
 var input_submit_mobile = document.getElementById("cta-form-mobile");
 
+
 input_submit.addEventListener("click",guardarContenido);
 input_submit_mobile.addEventListener("click",guardarContenido);
 flechita_atras.addEventListener("click",volverAtras);
@@ -14,10 +15,10 @@ flechita_atras_mobile.addEventListener("click",volverAtras);
 function guardarContenido(event){
     event.preventDefault();
 
-    var valor_nombre = input_nombre.value;
+    const valor_nombre = input_nombre.value;
     
-    window.valorPasado=valor_nombre;
-    window.location.href= "./agradecimiento.html?valor_nombre" + valor_nombre;
+    localStorage.setItem("nombreUsuario", valor_nombre);
+    window.location.href= "./agradecimiento.html";
     //window.location.href = "./agradecimiento.html";
 
     console.log(valor_nombre);
--- scriptHome.js
+++ scriptHome.js
@@ -1,10 +1,12 @@
 var b_home = document.getElementById("bHome");
 
 const valorRecibido = new URLSearchParams(window.location.search).get("valor_nombre");
-var nombre_usuario = document.getElementById("nombre-agradecimiento");
+var placeholderNombre = document.getElementById("nombre-agradecimiento");
+var nombre_usuario = localStorage.getItem("nombreUsuario");
 b_home.addEventListener("click", pasarAForm);
 
-nombre_usuario.innerHTML = valorRecibido;
+placeholderNombre.innerHTML = nombre_usuario;
+console.log(nombre_usuario);
 
 function pasarAForm (event){
     window.location.href = "./index.html";
